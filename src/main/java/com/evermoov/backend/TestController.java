9c45193d9974cc527f68c265426a3f5a71a4eb70
zsimon1977 <45688059+zsimon1977@users.noreply.github.com>
2026-03-24 10:02:14 +0100

    Update TestController.java

--- src/main/java/com/evermoov/backend/TestController.java
+++ src/main/java/com/evermoov/backend/TestController.java
@@ -1,7 +1,8 @@
 package com.evermoov.backend;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestController {
@@ -9,5 +10,21 @@ public class TestController {
     @GetMapping("/api/test")
     public String test() {
         return "OK";
+    }
+
+    @PostMapping("/api/calculate")
+    public Map<String, Double> calculate(@RequestBody Map<String, Double> input) {
+
+        double kezdo = input.get("kezdo");
+        double futamido = input.get("futamido");
+        double kamat = input.get("kamat");
+        double maradvany = input.get("maradvany");
+
+        double havi = (kezdo - maradvany) * (1 + kamat / 100) / futamido;
+
+        Map<String, Double> result = new HashMap<>();
+        result.put("havi", havi);
+
+        return result;
     }
 }
