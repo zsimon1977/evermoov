74ba5013b831b760d0fe20b3ed48f0b993a55f43
zsimon1977 <45688059+zsimon1977@users.noreply.github.com>
2026-03-24 09:00:15 +0100

    Update TestController.java

--- src/main/java/com/evermoov/backend/TestController.java
+++ src/main/java/com/evermoov/backend/TestController.java
@@ -1,1 +1,13 @@
+package com.evermoov.backend;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "OK";
+    }
+}
